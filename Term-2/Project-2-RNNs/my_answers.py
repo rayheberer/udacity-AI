a61301c46acf83c5f9849a9b2260bacfb7dd128f
Ray Heberer <ray.heberer@gmail.com>
2018-06-11 15:58:18 -0700

    Remove TODOs

--- Term-2/Project-2-RNNs/my_answers.py
+++ Term-2/Project-2-RNNs/my_answers.py
@@ -6,7 +6,7 @@ from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
+# fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
@@ -25,7 +25,7 @@ def window_transform_series(series, window_size):
 
     return X,y
 
-# TODO: build an RNN to perform regression on our time series input/output data
+# build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
     model = Sequential()
     
@@ -35,7 +35,7 @@ def build_part1_RNN(window_size):
     return model
 
 
-### TODO: return the text input with only ascii lowercase and the punctuation given below included.
+# return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
     punctuation = ['!', ',', '.', ':', ';', '?']
     lowercase = 'abcdefghijklmnopqrstuvwxyz'
@@ -46,7 +46,7 @@ def cleaned_text(text):
 
     return text
 
-### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
+# fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
@@ -58,7 +58,7 @@ def window_transform_text(text, window_size, step_size):
 
     return inputs, outputs
 
-# TODO build the required RNN model: 
+# build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
     model = Sequential()
